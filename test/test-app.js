137634690c721b55c1856c4bd5e949a00169871f
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-04-01 22:59:20 -0300

    fixing build, .css > .less

--- test/test-app.js
+++ test/test-app.js
@@ -70,7 +70,7 @@ describe('ng-fullstack:app', function () {
           'client/dev/favicon.png',
           'client/dev/index.html',
 
-          'client/dev/css/best_css_ever.css',
+          'client/dev/css/best_css_ever.less',
           'client/dev/css/events.less',
           'client/dev/css/fonts.less',
           'client/dev/css/frameworks_overrides.less',
