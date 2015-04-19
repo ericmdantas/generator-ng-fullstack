733e5e2e53062c4d8bd9b67f72d31bcdb97172f3
Joshua Godi <Joshua.Godi@nisc.coop>
2015-04-19 18:05:03 -0500

    refactor(ng-fullstack:resource): updating the resource sub generator to conform to the style guide

--- resource/templates/resource.js
+++ resource/templates/resource.js
@@ -1,17 +1,29 @@
-;(function(angular)
-{
-  "use strict";
+(function () {
+    "use strict";
 
-  angular
-    .module('myAwesomeApp')
-    .factory('<%= name %>Resource', ['$resource', function($resource)
-    {
-      var _url = '/api/<%= name %>/:id';
-      var _params = {id: '@id'};
-      var _method = {update: {method: 'PUT'}};
+    /**
+     * <%= name %>
+     */
+    angular
+        .module('myAwesomeApp')
+        .factory('<%= name %>Resource', <%= name %>Resource);
 
-      return $resource(_url, _params, _method);
-    }]);
+    <%= name %>Resource.$inject = ['$resource'];
 
-}(window.angular));
+    /* @ngInject */
+    function <%= name %>Resource($resource) {
+        var BASE_URL = '/api/<%= name %>';
+
+        var factory = {
+            getter: getter
+        };
+        return factory;
+
+        ////////////
+
+        function getter(id) {
+            return $resource(BASE_URL + '/:id', {id: id}, {update: {method: 'PUT'}});
+        }
+    }
+}());
 
