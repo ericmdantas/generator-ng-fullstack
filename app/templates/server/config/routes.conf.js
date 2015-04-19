ffe4c91c8d26af6ebf9a3242debfd7587c8c4810
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-04-19 11:24:27 -0300

    minor fixes

--- app/templates/server/config/routes.conf.js
+++ app/templates/server/config/routes.conf.js
@@ -7,11 +7,12 @@ import helmet from 'helmet';
 
 export default class RouteConfig
 {
-    static init(application, exp, dir)
+    static init(application, exp)
     {
         var _files = (process.env.NODE_ENV === 'production') ? '/client/dist/' : '/client/__tmp/';
+        var _root = process.cwd();
 
-        application.use(exp.static(dir + _files));
+        application.use(exp.static(_root + _files));
         application.use(bodyParser());
         application.use(morgan('dev'));
         application.use(contentLength.validateMax({max: 999}));
