cfd517bf8087dba7fda0cc83c9d601804d0c21d4
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-11-22 12:42:10 -0200

    more changes for ng2

--- app/templates/client_ng2/dev/todo/todo_cmp.ts
+++ app/templates/client_ng2/dev/todo/todo_cmp.ts
@@ -13,22 +13,23 @@ import {TodoService} from './todo_service.js';
 
 type Todo = {
   todoMessage: string;
-  id: number;
+  _id: string;
 }
 
 @Component({
   selector: 'todo-cmp',
-  templateUrl: 'client/dev/todo.html',
+  templateUrl: 'client/dev/todo/todo.html',
   providers: [TodoService],
   directives: [FORM_DIRECTIVES]
 })
 export class TodoCmp implements OnInit {
+  title: string = "Todo :D";
   todos: Todo[] = [];
   todoForm: ControlGroup;
 
   constructor(@Inject(FormBuilder) fb:FormBuilder, @Inject(TodoService) private _todoService: TodoService) {
     this.todoForm = fb.group({
-      todoMessage: ["", Validators.required]
+      "todoMessage": ["", Validators.required]
     });
   }
 
@@ -39,13 +40,17 @@ export class TodoCmp implements OnInit {
   private _getAll():void {
     this._todoService
         .getAll()
-        .subscribe(todos => this.todos = todos.json());
+        .subscribe((todos) => {
+          this.todos = todos.json();
+        });
   }
 
   add(message:string):void {
     this._todoService
         .add(message)
-        .subscribe(m => this.todos.push(m));
+        .subscribe((m) => {
+          this.todos.push(m);
+        });
   }
 
   remove(id:string|number):void {
@@ -53,9 +58,9 @@ export class TodoCmp implements OnInit {
       .remove(id)
       .subscribe(() => {
         this.todos.forEach((t, i) => {
-          if (t.id === id)
-            return this.todos.splice(1, i);
-        })
+          if (t._id === id)
+            return this.todos.splice(i, 1);
+        });
       })
   }
 }
--- app/templates/client_ng2/dev/todo/todo_service.ts
+++ app/templates/client_ng2/dev/todo/todo_service.ts
@@ -5,26 +5,23 @@ import {
 } from 'angular2/angular2';
 
 export class TodoService {
-  static ENDPOINT: string = '/api/todos/:id';  
+  static ENDPOINT: string = '/api/todos/:id';
 
   constructor(@Inject(Http) private _http: Http) {
 
   }
 
   getAll():Observable<any> {
-    return this._http
-               .get(TodoService.ENDPOINT.replace(':id', ''));
+    return this._http.get(TodoService.ENDPOINT.replace(':id', ''));
   }
 
   add(message:string):Observable<any> {
-    let _messageStringified = JSON.stringify({message});
+    let _messageStringified = JSON.stringify({todoMessage: message});
 
-    return this._http
-               .post(TodoService.ENDPOINT.replace(':id', ''), _messageStringified);
+    return this._http.post(TodoService.ENDPOINT.replace(':id', ''), _messageStringified);
   }
 
   remove(id: string|number):Observable<any> {
-    return this._http
-               .delete(TodoService.ENDPOINT.replace(':id', id));
+    return this._http.delete(TodoService.ENDPOINT.replace(':id', id));
   }
 }
