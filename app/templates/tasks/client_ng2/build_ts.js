844da496e6572205e60b73cb28003dc67a74ccde
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-11-30 16:19:32 -0200

    adds tsconfig to the gulp task #38

--- app/templates/tasks/client_ng2/build_ts.js
+++ app/templates/tasks/client_ng2/build_ts.js
@@ -1,10 +1,17 @@
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import cssmin from 'gulp-minify-css';
+import tsc from 'gulp-typescript';
 import {path} from './const';
 
-const INDEX_HTML = path.DEV + 'index.html';
+const TS = path.DEV + '**/*.ts';
+const TS_CONFIG = path.DEV + 'tsconfig.json';
 
 gulp.task('client.build_ts', () => {
+  let tsconfig = tsc.createProject('../../client/dev/tsconfig.json');
 
+  return tsconfig.src()
+                 .pipe(tsc(tsconfig))
+                 .js
+                 .pipe(gulp.dest('.'));
 });
