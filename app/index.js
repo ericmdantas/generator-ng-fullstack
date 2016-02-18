ead7e78f75b0c2d64623ad696af496b44f622e2f
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-02-18 21:34:17 -0200

    remove .es6 files from the entry points

--- app/index.js
+++ app/index.js
@@ -1,86 +1,46 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
-    value: true
-});
+const Base = require('yeoman-generator').Base;
+const chalk = require('chalk');
+const yosay = require('yosay');
+const MainGenerator = require('../_ng/full/generator').MainGenerator;
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+module.exports = class NgFullstack extends Base {
+    constructor(args, options, config) {
+        super(args, options, config);
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var _yeomanGenerator = require('yeoman-generator');
-
-var _chalk = require('chalk');
-
-var _chalk2 = _interopRequireDefault(_chalk);
-
-var _yosay = require('yosay');
-
-var _yosay2 = _interopRequireDefault(_yosay);
-
-var _ngFullGenerator = require('../_ng/full/generator');
-
-var NgFullstack = (function (_Base) {
-    _inherits(NgFullstack, _Base);
-
-    function NgFullstack(args, options, config) {
-        _classCallCheck(this, NgFullstack);
-
-        _get(Object.getPrototypeOf(NgFullstack.prototype), 'constructor', this).call(this, args, options, config);
-
-        this.generator = new _ngFullGenerator.MainGenerator(this);
+        this.generator = new MainGenerator(this);
     }
 
-    _createClass(NgFullstack, [{
-        key: 'initializing',
-        value: function initializing() {
-            this.pkg = require('../package.json');
-        }
-    }, {
-        key: 'prompting',
-        value: function prompting() {
-            this.generator.sayHello();
-        }
-    }, {
-        key: 'writing',
-        value: function writing() {
-            this.generator.writing();
-        }
-    }, {
-        key: 'install',
-        value: function install() {
-            this.generator.install();
-        }
-    }, {
-        key: 'prompUser',
-        value: function prompUser() {
-            this.generator.promptUser();
-        }
-    }, {
-        key: 'promptServer',
-        value: function promptServer() {
-            this.generator.promptServer();
-        }
-    }, {
-        key: 'promptClient',
-        value: function promptClient() {
-            this.generator.promptClient();
-        }
-    }, {
-        key: 'promptUserTranspilerServer',
-        value: function promptUserTranspilerServer() {
-            this.generator.promptTranspilerServer();
-        }
-    }]);
+    initializing() {
+      this.pkg = require('../package.json');
+    }
 
-    return NgFullstack;
-})(_yeomanGenerator.Base);
+    prompting() {
+      this.generator.sayHello();
+    }
 
-exports['default'] = NgFullstack;
-module.exports = exports['default'];
+    writing() {
+      this.generator.writing();
+    }
+
+    install() {
+      this.generator.install();
+    }
+
+    prompUser() {
+      this.generator.promptUser();
+    }
+
+    promptServer() {
+      this.generator.promptServer();
+    }
+
+    promptClient() {
+      this.generator.promptClient();
+    }
+
+    promptUserTranspilerServer() {
+      this.generator.promptTranspilerServer();
+    }
+}
--- service/index.js
+++ service/index.js
@@ -1,46 +1,18 @@
-'use strict';
+import {Base} from 'yeoman-generator';
+import {ServiceSubGenerator} from '../_ng/client/sub_generators_service';
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
+export default class ServiceGenerator extends Base {
+  constructor(args, options, config) {
+    super(args, options, config);
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var _yeomanGenerator = require('yeoman-generator');
-
-var _ngClientSub_generators_service = require('../_ng/client/sub_generators_service');
-
-var ServiceGenerator = (function (_Base) {
-  _inherits(ServiceGenerator, _Base);
-
-  function ServiceGenerator(args, options, config) {
-    _classCallCheck(this, ServiceGenerator);
-
-    _get(Object.getPrototypeOf(ServiceGenerator.prototype), 'constructor', this).call(this, args, options, config);
-
-    this.generator = new _ngClientSub_generators_service.ServiceSubGenerator(this);
+    this.generator = new ServiceSubGenerator(this);
   }
 
-  _createClass(ServiceGenerator, [{
-    key: 'initializing',
-    value: function initializing() {
-      this.generator.initializing();
-    }
-  }, {
-    key: 'writing',
-    value: function writing() {
-      this.generator.writing();
-    }
-  }]);
+  initializing() {
+    this.generator.initializing();
+  }
 
-  return ServiceGenerator;
-})(_yeomanGenerator.Base);
-
-exports['default'] = ServiceGenerator;
-module.exports = exports['default'];
+  writing() {
+    this.generator.writing();
+  }
+}
