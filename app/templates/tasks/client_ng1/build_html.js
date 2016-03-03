26f46c45b1a7f47b263bb9a5857d8e099895c14d
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-03-02 23:41:28 -0300

    adds image and html builds, #84

--- app/templates/tasks/client_ng1/build_html.js
+++ app/templates/tasks/client_ng1/build_html.js
@@ -1,9 +1,11 @@
 import gulp from 'gulp';
+import htmlmin from 'gulp-htmlmin';
 import {path, tasks} from './const';
 
 const VIEWS = path.DEV + '**/*.html';
 
 gulp.task(tasks.CLIENT_VIEWS_DIST, () => {
   return gulp.src(VIEWS, {base: path.DEV})
+			 .pipe(htmlmin({collapseWhitespace: true}))
              .pipe(gulp.dest(path.DIST));
 });
--- app/templates/tasks/client_ng1/build_image.js
+++ app/templates/tasks/client_ng1/build_image.js
@@ -1,9 +1,11 @@
 import gulp from 'gulp';
+import imageMin from 'gulp-imagemin';
 import {path, tasks} from './const';
 
 const IMAGES = path.DEV + 'common/images/*';
 
 gulp.task(tasks.CLIENT_IMAGE_DIST, () => {
   return gulp.src(IMAGES, {base: path.DEV})
+			       .pipe(imageMin())
              .pipe(gulp.dest(path.DIST));
 });
