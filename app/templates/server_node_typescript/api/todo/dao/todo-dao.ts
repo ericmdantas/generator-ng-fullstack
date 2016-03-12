429e590e444956c9144308ddc0c054c2e4bc64f4
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-03-11 22:20:33 -0300

    fix typescript server paths to the typings server, #79

--- app/templates/server_node_typescript/api/todo/dao/todo-dao.ts
+++ app/templates/server_node_typescript/api/todo/dao/todo-dao.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../typings/main.d.ts" />
+/// <reference path="../../../../typings/main.d.ts" />
 
 import * as mongoose from 'mongoose';
 import * as Promise from 'bluebird';
