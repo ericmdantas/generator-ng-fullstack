7dd7e6931d50687b225adb10ec573489f85cf05e
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-19 20:01:02 -0300

    adjusts exclusion of bower_components for ng1 build

--- app/templates/tasks/client_ng1/build_js.js
+++ app/templates/tasks/client_ng1/build_js.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import {path, tasks} from './const';
 
-const JS = [path.DIST + '**/*.js', path.dist + '!bower_components/**/*'];
+const JS = [path.DIST + '**/*.js', `!${path.dist}bower_components/**/*`];
 
 gulp.task(tasks.CLIENT_BUILD_JS_DIST, () => {
   return gulp.src(JS, {base: path.DIST})
