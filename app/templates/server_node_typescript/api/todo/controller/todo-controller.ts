7afed7ea7889f55f345e5f5946dd7ad0eff22e67
Tsaganos Tolis <apostolos94@gmail.com>
2016-04-24 01:22:48 +0300

    1.7.14

--- app/templates/server_node_typescript/api/todo/controller/todo-controller.ts
+++ app/templates/server_node_typescript/api/todo/controller/todo-controller.ts
@@ -4,7 +4,7 @@ import TodoDAO from '../dao/todo-dao';
 export class TodoController {
   static getAll(req: express.Request, res: express.Response):void {
       TodoDAO
-        .getAll()
+        ['getAll']()
         .then(todos => res.status(200).json(todos))
         .catch(error => res.status(400).json(error));
   }
@@ -13,7 +13,7 @@ export class TodoController {
       let _todo = req.body;
 
       TodoDAO
-        .createTodo(_todo)
+        ['createTodo'](_todo)
         .then(todo => res.status(201).json(todo))
         .catch(error => res.status(400).json(error));
   }
@@ -22,7 +22,7 @@ export class TodoController {
     let _id = req.params.id;
 
     TodoDAO
-      .deleteTodo(_id)
+      ['deleteTodo'](_id)
       .then(() => res.status(200).end())
       .catch(error => res.status(400).json(error));
   }
