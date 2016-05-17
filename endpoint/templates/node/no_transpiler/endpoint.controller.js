748026a8f03f8fd46b381cdd4932430c5d528fad
Jonno Schmidt <jonno@schmidtfam.us>
2016-05-17 00:00:07 -0700

    Remove usage of import in and change it to require

--- endpoint/templates/node/no_transpiler/endpoint.controller.js
+++ endpoint/templates/node/no_transpiler/endpoint.controller.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import <%= name %>DAO from '../dao/<%= name %>-dao';
+const <%= name %> = require('../dao/<%= name %>-dao');
 
 module.exports = class <%= name %>Controller {
   static getAll(req, res) {
