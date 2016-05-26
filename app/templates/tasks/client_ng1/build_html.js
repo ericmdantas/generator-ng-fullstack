0891adf4d06b419019d5434044d707d5d6e3e0ef
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-05-26 19:31:02 -0300

    client/build-dist - optimized html should be case sensitive, #151

--- app/templates/tasks/client_ng1/build_html.js
+++ app/templates/tasks/client_ng1/build_html.js
@@ -8,6 +8,9 @@ const VIEWS = path.DIST + '**/*.html';
 gulp.task(tasks.CLIENT_VIEWS_DIST, () => {
   return gulp.src(VIEWS, {base: path.DIST})
              .pipe(rev())
-			       .pipe(htmlmin({collapseWhitespace: true}))
+			       .pipe(htmlmin({
+               collapseWhitespace: true,
+               caseSensitive: true
+             }))
              .pipe(gulp.dest(path.DIST));
 });
