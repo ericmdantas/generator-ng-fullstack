5665a0b0e928e88d47832011eb9900bdf468f9cf
Marcin Mrotek <marcin.mrotek@stv.tv>
2016-06-23 15:55:43 +0100

    [FIX] Updated variable for tests separation

--- _ng/server/sub_generators_endpoint.js
+++ _ng/server/sub_generators_endpoint.js
@@ -18,7 +18,7 @@ exports.EndpointSubGenerator = class EndpointSubGenerator {
     this.wrapper.server = this.wrapper.config.get('server');
     this.wrapper.transpilerServer = this.wrapper.config.get('transpilerServer');
     this.wrapper.webFrameworkServer = this.wrapper.config.get('webFrameworkServer');
-    this.wrapper.tests = this.wrapper.config.get('tests');
+    this.wrapper.testsSeparated = this.wrapper.config.get('testsSeparated');
   }
 
   initializing() {
