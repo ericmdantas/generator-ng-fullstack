7d8afef1ca2b5e51ed7b7ebe5ec657d01097cde4
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-17 21:57:38 -0300

    initial commits for vue in client

---
+++ app/templates/client/vue/dev/todo/services/todo-service.ts
@@ -0,0 +1,36 @@
+function TodoService() {
+  static ENDPOINT: string = '/api/todos/:id';
+
+  constructor(@Inject(Http) private _http: Http) {
+
+  }
+
+  getAll():Observable<any> {
+    return this._http
+               .get(TodoService.ENDPOINT.replace(/:id/, ''))
+               .map((r) => r.json());
+  }
+
+  getById(id: string):Observable<any> {
+    return this._http
+               .get(TodoService.ENDPOINT.replace(/:id/, id))
+               .map((r) => r.json());
+  }
+
+  add(message:string):Observable<any> {
+    let _messageStringified = JSON.stringify({todoMessage: message});
+
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+
+    return this._http
+               .post(TodoService.ENDPOINT.replace(/:id/, ''), _messageStringified, {headers})
+               .map((r) => r.json());
+  }
+
+  remove(id: string):Observable<any> {
+    return this._http
+               .delete(TodoService.ENDPOINT.replace(/:id/, id));
+  }
+}
