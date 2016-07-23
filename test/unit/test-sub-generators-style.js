9e211862cb9ffaa0c6fc70dbda83c8b75bd39845
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-23 00:05:26 -0300

    implement subgenerator for style

--- test/unit/test-sub-generators-style.js
+++ test/unit/test-sub-generators-style.js
@@ -124,5 +124,47 @@ describe('StyleSubGenerator', () => {
         expect(_ssg.wrapper.template.calledWith(_firstCall[0], _firstCall[1])).to.be.true;
       });
     })
+
+    describe('vue2', () => {
+      it('should throw FeatureMissingError', () => {
+        let _gen = {
+          name: 'a',
+          options: {},
+          config: {
+            get() {
+              return 'vue2';
+            }
+          },
+          template: sinon.spy()
+        };
+
+        let _ssg = new StyleSubGenerator(_gen);
+
+        expect(() => _ssg.writing()).to.throw(Error, /Do it like this: --feature something-here/);
+      });
+
+      it('should have the writing called with the right stuff', () => {
+        let _gen = {
+          name: 'a',
+          options: {feature: 'c'},
+          config: {
+            get() {
+              return 'vue2';
+            }
+          },
+          template: sinon.spy()
+        };
+
+        let _ssg = new StyleSubGenerator(_gen);
+
+        _ssg.writing();
+
+        let _firstCall = ['style.css', knownPaths.PATH_CLIENT_FEATURES + _gen.options.feature + '/styles/' + _gen.name + '.css'];
+
+        expect(_ssg.wrapper.writing).to.have.been.called;
+        expect(_ssg.wrapper.template.calledWith(_firstCall[0], _firstCall[1])).to.be.true;
+      });
+    })
+
   });
 });
