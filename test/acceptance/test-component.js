b5ca974c37875423babfa84796232088a3f64ac2
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-23 16:55:51 -0300

    implements test for vue component, #194

--- test/acceptance/test-component.js
+++ test/acceptance/test-component.js
@@ -5,7 +5,7 @@ import {MockConfigFile} from '../helpers/mocks';
 
 describe('subgenerator -> component', () => {
   describe('ng2', () => {
-    describe('component - testsSeparated is true', () => {
+    describe('testsSeparated is true', () => {
       before(function (done) {
         helpers
           .run(path.join(__dirname, '../../component'))
@@ -32,7 +32,7 @@ describe('subgenerator -> component', () => {
       });
     });
 
-    describe('NgFullstack:component - testsSeparated is false', () => {
+    describe('testsSeparated is false', () => {
       before(function (done) {
         helpers
           .run(path.join(__dirname, '../../component'))
@@ -59,4 +59,59 @@ describe('subgenerator -> component', () => {
       });
     });
   })
+
+  describe('vue2', () => {
+    describe('testsSeparated is true', () => {
+      before(function (done) {
+        helpers
+          .run(path.join(__dirname, '../../component'))
+          .inTmpDir(function(dir) {
+            MockConfigFile.create({
+              "generator-ng-fullstack": {
+                "client": "vue2",
+                "testsSeparated": true
+              }
+            }, this.async());
+          })
+          .withArguments('user')
+          .withOptions({ 'skip-install': true, feature: 'yo'})
+          .on('end', done);
+      });
+
+      it('creates files', () => {
+        assert.file([
+            'client/dev/yo/components/user.js',
+            'client/dev/yo/styles/user.css',
+            'tests/client/yo/components/user_test.js'
+          ]);
+      });
+    });
+
+    describe('testsSeparated is false', () => {
+      before(function (done) {
+        helpers
+          .run(path.join(__dirname, '../../component'))
+          .inTmpDir(function(dir) {
+            MockConfigFile.create({
+              "generator-ng-fullstack": {
+                "client": "vue2",
+                "testsSeparated": false
+              }
+            }, this.async());
+          })
+          .withArguments('user')
+          .withOptions({ 'skip-install': true, feature: 'yo'})
+          .on('end', done);
+      });
+
+      it('creates files', () => {
+        assert.file([
+            'client/dev/yo/components/user.js',
+            'client/dev/yo/styles/user.css',
+            'client/dev/yo/components/user_test.js'
+          ]);
+      });
+    });
+  })
+
 })
