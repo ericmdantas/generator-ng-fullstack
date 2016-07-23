07373588faa05b0cf0462e763e8e27344c273b71
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-22 23:36:31 -0300

    implements subgenerator for factory, #190

--- test/unit/test-sub-generators-factory.js
+++ test/unit/test-sub-generators-factory.js
@@ -189,5 +189,64 @@ describe('FactorySubGenerator', () => {
         expect(_fsg.wrapper.template.calledWith(_secondCall[0], _secondCall[1], _secondCall[2])).to.be.true;
       });
     });
+
+    describe('vue2', () => {
+      it('should throw FeatureMissingError', () => {
+        let _gen = {
+          name: 'a',
+          options: {},
+          config: {
+            get() {return 'vue2'}
+          },
+          template: sinon.spy()
+        };
+
+        sinon.mock(_gen.template);
+
+        let _fsg = new FactorySubGenerator(_gen);
+
+        expect(() => _fsg.writing()).to.throw(Error, /Do it like this: --feature something-here/);
+      });
+
+      it('should have the writing called with the right stuff - testsSeparated', () => {
+        let _gen = {
+          name: 'a',
+          options: {feature: 'c'},
+          testsSeparated: false,
+          config: {
+            get(token) {
+              switch (token) {
+                case "testsSeparated": return false;
+                default: return 'vue2';
+              }
+            }
+          },
+          template: sinon.spy()
+        };
+
+        sinon.mock(_gen.template);
+
+        let _fsg = new FactorySubGenerator(_gen);
+
+        _fsg.writing();
+
+        let _firstCall = [
+          'vue2/factory.js',
+          knownPaths.PATH_CLIENT_FEATURES + _gen.options.feature + '/factory/' + _gen.name + '.js', {
+            name: utils.capitalizeFirst(_gen.name)
+          }];
+
+        let _secondCall = [
+          'vue2/factory_test.js',
+          knownPaths.PATH_CLIENT_FEATURES + _gen.options.feature + '/factory/' + _gen.name + '_test.js', {
+            name: utils.capitalizeFirst(_gen.name)
+          }];
+
+        expect(_fsg.wrapper.writing).to.have.been.called;
+        expect(_fsg.wrapper.template.calledWith(_firstCall[0], _firstCall[1], _firstCall[2])).to.be.true;
+        expect(_fsg.wrapper.template.calledWith(_secondCall[0], _secondCall[1], _secondCall[2])).to.be.true;
+      });
+    });
+
   });
 });
