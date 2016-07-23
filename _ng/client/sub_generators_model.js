9d48c1af8ee6c234f2f3cd20fcddaaba81b70d95
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-22 23:47:00 -0300

    implements subgenerator for model, #190

--- _ng/client/sub_generators_model.js
+++ _ng/client/sub_generators_model.js
@@ -3,6 +3,7 @@
 const optionsParser = require('../utils/options_parser');
 const FeatureMissingError = require('../utils/errors').FeatureMissingError;
 const AngularFactory = require('./angular').AngularFactory;
+const VueFactory = require('./vue').VueFactory;
 
 exports.ModelSubGenerator = class ModelSubGenerator {
   constructor(generator) {
@@ -22,11 +23,18 @@ exports.ModelSubGenerator = class ModelSubGenerator {
 
   writing() {
     let _feature = optionsParser.getFeature(this.wrapper.options);
+    let _client = this.wrapper.client;
 
     if (!_feature.length) {
       throw new FeatureMissingError();
     }
 
-    AngularFactory.build(this.wrapper.client, this.wrapper).copyModel();
+    if ((_client === AngularFactory.tokens().NG1) || (_client === AngularFactory.tokens().NG2)) {
+      return AngularFactory.build(this.wrapper.client, this.wrapper).copyModel();
+    }
+
+    if (_client === VueFactory.tokens().VUE2) {
+      return VueFactory.build(this.wrapper.client, this.wrapper).copyModel();
+    }
   }
 };
