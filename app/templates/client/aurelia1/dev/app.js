c099ee3045437bae931a0fbc070b740ea8e1d7cd
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-27 22:03:33 -0300

    adds initial implementation for aurelia client, #175

---
+++ app/templates/client/aurelia1/dev/app.js
@@ -0,0 +1,9 @@
+export class App {
+    configureRouter(config, router) {
+        config.map([
+            {route: ['', 'todo'], moduleId: './todo/components/todo'}
+        ]);
+
+        this.router = router;
+    }
+}
---
+++ app/templates/client/aurelia1/dev/todo/services/todo-service.js
@@ -0,0 +1,28 @@
+import {HttpClient} from 'aurelia-http-client';
+import {Todo} from '../models/todo';
+import {inject} from 'aurelia-framework';
+
+@inject(HttpClient)
+export class TodoService {
+    constructor(http) {
+        this.http = http;
+    }
+
+    getAll() {
+        return this.http.get('/api/todos')
+            .then((todos) => {
+                return todos.map((t) => new Todo(t)); 
+            });
+    }
+
+    add(todo) {
+        return this.http.post('/api/todos', todo)
+            .then((t) => {
+                return new Todo(t);
+            });
+    }
+
+    remove(id) {
+        return this.http.delete('/api/todos/' + id);
+    }
+}
