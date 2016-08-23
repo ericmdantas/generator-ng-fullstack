7cdfc0097b5fbc75b9eaa3be5027da8581455eb2
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-08-23 07:55:57 -0300

    adjusts task watch for the client, so less/sass can be added easily, #25

--- app/templates/tasks/client/ng2/watch.js
+++ app/templates/tasks/client/ng2/watch.js
@@ -2,26 +2,31 @@ import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import {path, tasks} from './const';
 
-const TS = path.DEV + '**/*.ts';
-const CSS = path.DEV + '**/*.css';
-const HTML = path.DEV + '**/*.html';
-
 gulp.task(tasks.CLIENT_RELOAD, () => {
   return browserSync.reload();
 });
 
 gulp.task(tasks.CLIENT_WATCH, [tasks.CLIENT_BUILD_TS], () => {
   <% if (!!secure) { %>
-  browserSync({proxy: "https://localhost:3333", reloadDelay: 1000});
+  browserSync({
+    proxy: "https://localhost:3333", 
+    reloadDelay: 1000
+  });
   <%} else {%>
-  browserSync({proxy: "http://localhost:3333", reloadDelay: 1000});
+  browserSync({
+    proxy: "http://localhost:3333", 
+    reloadDelay: 1000
+  });
   <% } %>
 
   let _watchable = [];
 
-  _watchable.push(TS);
-  _watchable.push(CSS);
-  _watchable.push(HTML);
+  _watchable.push(path.DEV + '**/.ts');
+  _watchable.push(path.DEV + '**/*.css');
+  _watchable.push(path.DEV + '**/*.html');
 
-  return gulp.watch(_watchable, [tasks.CLIENT_BUILD_TS, tasks.CLIENT_RELOAD]);
+  return gulp.watch(_watchable, [
+    tasks.CLIENT_BUILD_TS, 
+    tasks.CLIENT_RELOAD
+  ]);
 });
