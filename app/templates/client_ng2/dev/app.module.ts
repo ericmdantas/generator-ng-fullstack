7d98edd4f9a8b66d7c29fa09a289711863dd70f6
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-09-06 07:28:02 -0300

    starts to make changes for ng2 rc6, #217

--- app/templates/client_ng2/dev/app.module.ts
+++ app/templates/client_ng2/dev/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { FormsModule, REACTIVE_FORM_DIRECTIVES, FormBuilder } from '@angular/forms';
+import { FormsModule, FormBuilder } from '@angular/forms';
 import { BrowserModule  } from '@angular/platform-browser';
 import { TodoCmp }   from './todo/components/todo-cmp';
 import { TodoService }   from './todo/services/todo-service';
