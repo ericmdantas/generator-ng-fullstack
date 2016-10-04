4ae96078af58f03a3983337bf7fe4a65dadd5631
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-04 12:47:47 -0300

    minor format change

--- app/templates/tasks/client/ng1/build_js.js
+++ app/templates/tasks/client/ng1/build_js.js
@@ -2,7 +2,10 @@ import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import {base, tasks} from './const';
 
-const JS = [base.DIST + '**/*.js', `!${base.DIST}bower_components/**/*`];
+const JS = [
+  base.DIST + '**/*.js', 
+  '!' + base.DIST + 'bower_components/**/*'
+];
 
 gulp.task(tasks.CLIENT_BUILD_JS_DIST, () => {
   return gulp.src(JS, {base: base.DIST})
