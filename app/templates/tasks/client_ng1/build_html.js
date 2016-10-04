3eec5c1b05c0dd9bd4dcaadc93cd1370fe9b6e62
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-04 12:45:22 -0300

    ng1 - dont try to parse htmls inside dist/bower_components

--- app/templates/tasks/client_ng1/build_html.js
+++ app/templates/tasks/client_ng1/build_html.js
@@ -3,7 +3,10 @@ import htmlmin from 'gulp-htmlmin';
 import rev from 'gulp-rev-append';
 import {path, tasks} from './const';
 
-const VIEWS = path.DIST + '**/*.html';
+const VIEWS = [
+  path.DIST + '**/*.html',
+  '!' + path.DIST + 'bower_components/**/*.html'
+];
 
 gulp.task(tasks.CLIENT_VIEWS_DIST, () => {
   return gulp.src(VIEWS, {base: path.DIST})
--- app/templates/tasks/client_ng1/build_image.js
+++ app/templates/tasks/client_ng1/build_image.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import imageMin from 'gulp-imagemin';
 import {path, tasks} from './const';
 
-const IMAGES = path.DIST + 'common/images/*';
+const IMAGES = base.DIST + '**/*.{png,jpg,jpeg,svg,gif}';
 
 gulp.task(tasks.CLIENT_IMAGE_DIST, () => {
   return gulp.src(IMAGES, {base: path.DIST})
