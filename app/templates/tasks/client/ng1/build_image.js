47cc0f440d21ad0d614a227ccc76bec324df1ae7
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-07 09:50:23 -0300

    tasks-images - dont optimize bower_components images

--- app/templates/tasks/client/ng1/build_image.js
+++ app/templates/tasks/client/ng1/build_image.js
@@ -2,7 +2,10 @@ import gulp from 'gulp';
 import imageMin from 'gulp-imagemin';
 import {base, tasks} from './const';
 
-const IMAGES = base.DIST + '**/*.{png,jpg,jpeg,svg,gif}';
+const IMAGES = [
+	base.DIST + '**/*.{png,jpg,jpeg,gif}',
+	'!' + base.DIST + 'bower_components/**/*.{png,jpg,jpeg,gif}',
+];
 
 gulp.task(tasks.CLIENT_IMAGE_DIST, () => {
   return gulp.src(IMAGES, {base: base.DIST})
