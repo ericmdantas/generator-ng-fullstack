bdbacc9de6da70355ede81fdd1682f540de5ab3e
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-30 19:13:21 -0200

    bumps gulp-typescript version to 3.x

--- app/templates/tasks/client_ng2/build_ts.js
+++ app/templates/tasks/client_ng2/build_ts.js
@@ -5,10 +5,10 @@ import {path, tasks} from './const';
 const TS_CONFIG = path.ROOT + 'tsconfig.json';
 
 gulp.task(tasks.CLIENT_BUILD_TS, () => {
-  let tsconfigSrc = tsc.createProject(TS_CONFIG);
+  let _tsProject = tsc.createProject(TS_CONFIG);
 
-  return tsconfigSrc.src()
-                    .pipe(tsc(tsconfigSrc))
-                    .js
-                    .pipe(gulp.dest('.'));
+  return tsProject.src()
+                   .pipe(_tsProject())
+                   .js
+                   .pipe(gulp.dest('.'));
 });
