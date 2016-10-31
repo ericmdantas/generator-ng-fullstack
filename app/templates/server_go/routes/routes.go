52cd812fbabaf7ed8bc31a1add39908b54bbddb5
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-31 07:23:47 -0200

    adds compression middleware for the servers, closes #230

--- app/templates/server_go/routes/routes.go
+++ app/templates/server_go/routes/routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"compress/gzip"
 	"github.com/<%= username %>/<%= appName %>/server/api/todo/routes"
 	<% if (!differentStaticServer) {%>
 	"github.com/<%= username %>/<%= appName %>/server/common/static"
@@ -11,6 +12,9 @@ import (
 
 func Init(e *echo.Echo) {
 	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: gzip.BestCompression,
+	})	
 	<% if (!differentStaticServer) {%>
 	static.Init(e)
 	<% } %>
--- app/templates/server_node_babel/config/routes.conf.js
+++ app/templates/server_node_babel/config/routes.conf.js
@@ -3,12 +3,19 @@ import bodyParser from 'body-parser';
 import contentLength from 'express-content-length-validator';
 import helmet from 'helmet';
 import express from 'express';
+import compression from 'compression';
+import zlib from 'zlib';
 
 export default class RouteConfig {
     static init(application) {
         let _root = process.cwd();
         let _nodeModules = '/node_modules/';
         let _clientFiles = (process.env.NODE_ENV === 'production') ? '/client/dist/' : '/client/dev/';
+        
+        application.use(compression({
+            level: zlib.Z_BEST_COMPRESSION,
+            threshold: '1kb'
+        }));
 
         application.use(express.static(_root + _nodeModules));
         application.use(express.static(_root + _clientFiles));
