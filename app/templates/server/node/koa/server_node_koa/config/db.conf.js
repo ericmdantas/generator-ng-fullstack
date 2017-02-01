4d015694b2f129f81ed171bae78b6febefb1a123
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-02-01 16:21:04 -0200

    uses bluebird's promises in mongoose

--- app/templates/server/node/koa/server_node_koa/config/db.conf.js
+++ app/templates/server/node/koa/server_node_koa/config/db.conf.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const Promise = require('bluebird');
 const dbConst = require("../constants/db.json");
 
 module.exports = class DBConfig {
@@ -8,6 +9,7 @@ module.exports = class DBConfig {
       const URL = (process.env.NODE_ENV === "production") ? process.env.MONGOHQ_URL
                                                           : dbConst.localhost;
 
+      mongoose.Promise = Promise;
       mongoose.connect(URL);
       mongoose.connection.on("error", console.error.bind(console, "An error ocurred with the DB connection: "));
     }
