678e1a916bf92aab7fc044b6f131190a051fc557
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-02-03 09:10:59 -0200

    node-typescript-server: uses require instead of import in db config

--- app/templates/server/node/koa/server_node_koa_typescript/config/db.conf.ts
+++ app/templates/server/node/koa/server_node_koa_typescript/config/db.conf.ts
@@ -2,14 +2,15 @@
 
 import * as mongoose from "mongoose";
 import * as Promise from "bluebird";
-import * as dbConst from "../constants/db.json";
+
+const dbConst = require("../constants/db.json");
 
 export default class DBConfig {
     static init() {
       const URL = (process.env.NODE_ENV === "production") ? process.env.MONGOHQ_URL
                                                           : dbConst.localhost;
 
-      (<any>mongoose).Promise = Promise;                                                    
+      (<any>mongoose).Promise = Promise;
       mongoose.connect(URL);
       mongoose.connection.on("error", console.error.bind(console, "An error ocurred with the DB connection: "));
     }
