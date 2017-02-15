3b827fe56c423d497214430d64eb547c7e5f14e4
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-02-15 10:50:16 -0200

    fixes gin's controller callback

--- app/templates/server/go/gin/api/todo/controller/todocontroller.go
+++ app/templates/server/go/gin/api/todo/controller/todocontroller.go
@@ -7,34 +7,34 @@ import (
 	"net/http"
 )
 
-func GetAll(c *gin.Context) error {
+func GetAll(c *gin.Context) {
 	ts, _ := tododao.All()
 
-	return c.JSON(http.StatusOK, ts)
+	c.JSON(http.StatusOK, ts)
 }
 
-func GetById(c *gin.Context) error {
+func GetById(c *gin.Context) {
 	id := c.Param("id")
 
 	nt, _ := tododao.GetById(id)
 
-	return c.JSON(http.StatusOK, nt)
+	c.JSON(http.StatusOK, nt)
 }
 
-func NewTodo(c *gin.Context) error {
+func NewTodo(c *gin.Context) {
 	t := new(todo.Todo)
 
 	c.Bind(t)
 
 	nt, _ := tododao.NewTodo(*t)
 
-	return c.JSON(http.StatusOK, nt)
+	c.JSON(http.StatusOK, nt)
 }
 
-func RemoveTodo(c *gin.Context) error {
+func RemoveTodo(c *gin.Context) {
 	id := c.Param("id")
 
 	tododao.DeleteTodo(id)
 
-	return c.String(http.StatusOK, "")
+	c.String(http.StatusOK, "")
 }
