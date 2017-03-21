6bd1b6f427e4547f8ebc186308f6e3273ffcc8e7
Bernardo Esbérard <bernardo.dsn.erp@alterdata.com.br>
2017-03-21 10:48:45 -0300

    Adjusts app.config for cordova applications.

--- app/templates/client/ng1/dev/app.config.js
+++ app/templates/client/ng1/dev/app.config.js
@@ -5,9 +5,14 @@
     .config([
       "$locationProvider",
       function($locationProvider) {
+        <% if (!cordova) { %>
         $locationProvider
           .html5Mode(true)
           .hashPrefix('*');
+        <% } else { %>
+        $locationProvider
+          .html5Mode(false);
+        <% } %>
       }
     ]);
 }(window.angular));
