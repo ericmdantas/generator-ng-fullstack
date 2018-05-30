cded812773b24e49a948d44ea6f365b7af257193
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-05-30 16:59:57 -0300

    comments vue's unit_test for now

--- app/templates/tasks/client/vue2/index.js
+++ app/templates/tasks/client/vue2/index.js
@@ -5,7 +5,7 @@ import {base, tasks} from "./const";
 gulp.task(tasks.CLIENT_BUILD_DIST, () => {
   return new Promise((resolve, reject) => {
     runSequence(
-                  tasks.CLIENT_UNIT_TEST,
+                  // tasks.CLIENT_UNIT_TEST,
                   tasks.CLIENT_DEL_DIST,
                   tasks.CLIENT_COPY,
                   tasks.CLIENT_VIEWS_DIST,
