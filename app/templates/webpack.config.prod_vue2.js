5d0804a5dfa63cafcde45909f6111d5b6377df8f
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-06-04 15:22:07 -0300

    adds some stuff to vue2's webpack prod config - #290

--- app/templates/webpack.config.prod_vue2.js
+++ app/templates/webpack.config.prod_vue2.js
@@ -3,6 +3,7 @@ const path = require('path')
 const webpackMerge = require('webpack-merge')
 const devConfig = require('./webpack.config.dev.js')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
@@ -29,6 +30,7 @@ module.exports = webpackMerge(devConfig, {
     }
   },
   plugins: [
+    new CleanWebpackPlugin(['client/dist']),
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
