c72df87e3b7849d5bea15468b895b7f493a984c8
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-07-20 16:35:44 -0300

    adds file-loader to fonts in vue's webpack config

--- app/templates/webpack.config.dev_vue2.js
+++ app/templates/webpack.config.dev_vue2.js
@@ -72,7 +72,17 @@ module.exports = {
         use: [
           'file-loader',
         ]
-      }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            }
+        }]
+      },
     ]
   },
   plugins: [    
